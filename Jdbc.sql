791ea95932c00727eed7f768ba7a074e02c6e15f
Emre Gondermez <emregondermez@gmail.com>
2022-01-10 21:56:29 +0100

    last

--- Jdbc.sql
+++ Jdbc.sql
@@ -1,20 +1,42 @@
 use sys;
-CREATE TABLE personel
-    (
-        id int PRIMARY KEY , 
-        isim VARCHAR(50), 
-        sehir VARCHAR(50), 
-        maas int, 
-        sirket VARCHAR(20)
-        
+CREATE TABLE bolumler (
+      bolum_id   int PRIMARY KEY,
+      bolum_isim VARCHAR(14),
+      konum      VARCHAR(13)
     );
+    
+    INSERT INTO bolumler VALUES (10,'MUHASEBE','IST');
+    INSERT INTO bolumler VALUES (20,'MUDURLUK','ANKARA');
+    INSERT INTO bolumler VALUES (30,'SATIS','IZMIR');
+    INSERT INTO bolumler VALUES (40,'ISLETME','BURSA');
+    INSERT INTO bolumler VALUES (50,'DEPO', 'YOZGAT');
 
-
-    INSERT INTO personel VALUES(123456789, 'Ali Yilmaz', 'Istanbul', 5500, 'Honda');
-    INSERT INTO personel VALUES(234567890, 'Veli Sahin', 'Istanbul', 4500, 'Toyota');
-    INSERT INTO personel VALUES(345678901, 'Mehmet Ozturk', 'Ankara', 3500, 'Honda'); 
-    INSERT INTO personel VALUES(456789012, 'Mehmet Ozturk', 'Izmir', 6000, 'Ford');
-    INSERT INTO personel VALUES(567890123, 'Mehmet Ozturk', 'Ankara', 7000, 'Tofas');
-    INSERT INTO personel VALUES(456715012, 'Veli Sahin', 'Ankara', 4500, 'Ford');
+    CREATE TABLE personel (
+      id   int PRIMARY KEY,
+      isim VARCHAR(10),
+      meslek        VARCHAR(9),
+      mudur_id      int,
+      maas          int,
+      bolum_id      int
+    );
+    
+ 
+  
+    INSERT INTO personel VALUES (7369,'AHMET','KATIP',1111,800,20);
+    INSERT INTO personel VALUES (7499,'BAHATTIN','SATISE',1222,1600,30);
+    INSERT INTO personel VALUES (7521,'NESE','SATISE',1222,1250,30);
+    INSERT INTO personel VALUES (1111,'MUZAFFER','MUDUR',7839,2975,20);
+    INSERT INTO personel VALUES (7654,'MUHAMMET','SATISE',1222,1250,30);
+    INSERT INTO personel VALUES (1222,'EMINE','MUDUR',7839,2850,30);
+    INSERT INTO personel VALUES (1333,'HARUN','MUDUR',7839, 2450,10);
+    INSERT INTO personel VALUES (7788,'MESUT','ANALIST',1111,3000,20);
+    INSERT INTO personel VALUES (7839,'SEHER','BASKAN',NULL,5000,10);
+    INSERT INTO personel VALUES (7844,'DUYGU','SATISE',1222,1500,30);
+    INSERT INTO personel VALUES (7876,'ALI','KATIP',1111,1100,20);
+    INSERT INTO personel VALUES (7900,'MERVE','KATIP',1222,950,30);
+    INSERT INTO personel VALUES (7902,'NAZLI','ANALIST',1111,3000,20);
+    INSERT INTO personel VALUES (7934,'EBRU','KATIP',1333,1300,10);
+    INSERT INTO personel VALUES (7956,'SIBEL','MIMAR',1333,3300,60);
+    INSERT INTO personel VALUES (7933,'ZEKI','MUHENDIS',1333,4300,60);
     
     commit;
